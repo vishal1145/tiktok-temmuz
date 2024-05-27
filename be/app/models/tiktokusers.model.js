4948d33c337293ef94c0b7f42fa208a77c39c5a8
vishal1145 <vishal1145@gmail.com>
2024-05-27 19:52:05 +0530

    add role key

--- be/app/models/tiktokusers.model.js
+++ be/app/models/tiktokusers.model.js
@@ -2,6 +2,7 @@ const mongoose  = require("mongoose");
 const memberSchema = new mongoose.Schema({
   contact_number: { type: String },
   otp : { type: String },
+  role : { type: String }
 })
 
 module.exports = mongoose.model("member", memberSchema);
