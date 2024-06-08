f098cc7d4f1ea7016b44daa7a300990c11674982
vishal1145 <vishal1145@gmail.com>
2024-06-08 16:11:27 +0530

    fixed

--- be/app/services/payment.service.js
+++ be/app/services/payment.service.js
@@ -1,32 +1,40 @@
-const Payment = require("../models/payment");
+const Payment = require("../models/payment.model");
+const User = require("../models/tiktokusers.model");
 const mongoose = require("mongoose");
 
 exports.createPayment = async (paymentData) => {
-  const { user_name, request_date, amount, status, notes, action, user_id } = paymentData;
+  const { amount, status, notes, user_id } = paymentData;
   const newPayment = new Payment({
-    user_name,
-    request_date,
     amount,
     status,
     notes,
-    action,
     user_id
   });
   await newPayment.save();
   return newPayment;
 };
 
-exports.getPayments = () => {
+exports.getPayments = async (_id) => {
+
+  const user = await User.findById(_id);
+
+  if (user.role == "admin") {
     return Payment.find().populate("user_id");
-  };
+  } else if (user.role == "user") {
+    const pipeline = [
+      { $match: { user_id: new mongoose.Types.ObjectId(_id) } },
+    ];
+    return Payment.aggregate(pipeline);
+  }
+};
 
 
-  exports.getOnlyUser = async (_id) => {
-    const publishers = await Payment.aggregate([{ $match: { user_id: new mongoose.Types.ObjectId(_id) } }]);
-    return publishers;
-  };
-  
-  
+exports.getOnlyUser = async (_id) => {
+  const publishers = await Payment.aggregate([{ $match: { user_id: new mongoose.Types.ObjectId(_id) } }]);
+  return publishers;
+};
+
+
 
 exports.getPaymentById = async (id) => {
   const payment = await Payment.findById(id);
@@ -35,7 +43,7 @@ exports.getPaymentById = async (id) => {
 
 exports.updatePaymentStatus = async (id, updateData) => {
   const { status } = updateData;
-  const payment = await Payment.findByIdAndUpdate(id,{ status });
+  const payment = await Payment.findByIdAndUpdate(id, { status });
   return payment;
 };
 
