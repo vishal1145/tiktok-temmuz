9283a28fe823f5de6aca0456eb4de4b57f4eabd1
vishal1145 <vishal.kumar1145@gmail.com>
2024-06-08 17:47:03 +0530

    commission api push

--- be/app/controllers/commission.controller.js
+++ be/app/controllers/commission.controller.js
@@ -3,6 +3,8 @@ const commission = require('../models/commission.model');
 // Create a new commission
 exports.createCommission = async (req, res) => {
     try {
+        // 
+        await commission.deleteMany({});
         const commission_data = new commission(req.body);
         await commission_data.save();
         res.status(201).json(commission_data);
@@ -14,7 +16,7 @@ exports.createCommission = async (req, res) => {
 // Get all commission
 exports.getAllCommission = async (req, res) => {
     try {
-        const commissions = await commission.find();
+        const commissions = await commission.findOne();
         res.json(commissions);
     } catch (error) {
         res.status(500).json({ message: error.message });
--- be/app/models/commission.model.js
+++ be/app/models/commission.model.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 // this is not in use 
 const commissionSchema = new mongoose.Schema({
-  first_commission: { type: String, default: NULL },
-  second_commission: { type: String, default: NULL },
+  first_commission: { type: String, default: 1 },
+  second_commission: { type: String, default: 2 },
   // uid: { type: String,unique: true},
   
 });
